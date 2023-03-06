68a78db25a37d5ff53ed4a1a96860437cc1f4785
lpcci <lorenzo.petrocchi@hotmail.fr>
2023-03-06 16:02:11 +0100

    routing OK

--- src/app/private/private-routing.module.ts
+++ src/app/private/private-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'vehicles',
+    loadChildren: () =>
+      import('./vehicles/vehicles.module').then((m) => m.VehiclesModule),
+  },
+  {
+    path: 'lists',
+    loadChildren: () =>
+      import('./lists/lists.module').then((m) => m.ListsModule),
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/vehicles',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
