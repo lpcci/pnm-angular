91e0d6a3f5f6f480aa88f476f6ff31e43bd6057f
Lorenzo PETROCCHI <lorenzo.petrocchi@e-mothep.com>
2023-03-07 16:59:43 +0100

    header OK

--- src/app/core/components/header-bar/header-bar.component.ts
+++ src/app/core/components/header-bar/header-bar.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
-  styleUrls: ['./header-bar.component.scss']
+  styleUrls: ['./header-bar.component.scss'],
 })
 export class HeaderBarComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 const MODULES = [
   CommonModule,
+  RouterModule,
   MatToolbarModule,
   MatIconModule,
-  MatButtonModule
-]
-
+  MatButtonModule,
+];
 
 @NgModule({
   declarations: [],
   imports: [MODULES],
-  exports: [MODULES]
+  exports: [MODULES],
 })
-export class SharedModule { }
+export class SharedModule {}
